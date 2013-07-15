74ab9dd7c367063fe16e0f34a1adb29601524882
Richard Shin <skunkworks@gmail.com>
2013-07-15 11:13:11 -0700

    Added FlickrFetcher

---
+++ SPoT/FlickrFetcher/FlickrAPIKey.h
@@ -0,0 +1,12 @@
+//
+//  FlickrAPIKey.h
+//
+//  Created for Stanford CS193p Winter 2013.
+//  Copyright 2013 Stanford University
+//  All rights reserved.
+//
+//  Get your own key!
+//  No Flickr fetches will work without the API Key!
+//
+
+#define FlickrAPIKey @"b0fd7aa68be97ca62864e02a30e352ee"
