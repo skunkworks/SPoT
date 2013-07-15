03837ae50c4cf278e36132a2c16723d159c05c44
Richard Shin <skunkworks@gmail.com>
2013-07-15 12:18:52 -0700

    Title and subtitle working

--- SPoT/StanfordPhotosTVC.m
+++ SPoT/StanfordPhotosTVC.m
@@ -10,7 +10,8 @@
 #import "FlickrFetcher.h"
 
 @interface StanfordPhotosTVC ()
-@property (strong, nonatomic) NSMutableDictionary *photosByTags; // NSString key, NSNumber value
+@property (strong, nonatomic) NSMutableDictionary *tags; // Key = NSString, Value = NSArray of NSDictionary
+@property (strong, nonatomic) NSArray *rowTags; // of NSString
 @end
 
 @implementation StanfordPhotosTVC
@@ -19,53 +20,56 @@
 
 - (void)setPhotos:(NSArray *)photos {
     _photos = photos;
-    // Any time photos gets updated, we need to update photosByTags as well.
-    _photosByTags = nil;
+    // Any time photos gets updated, we need to update tags as well
+    _tags = nil;
     [self.tableView reloadData];
 }
 
-- (NSDictionary *)photosByTags {
-    if (!_photosByTags) {
-        _photosByTags = [[NSMutableDictionary alloc] init];
+// Key is the tag name, value is an array of Flickr photo metadata (as NSDictionary objects)
+- (NSDictionary *)tags {
+    if (!_tags) {
+        _tags = [[NSMutableDictionary alloc] init];
 
-        // self.photo is an NSArray of NSDictionary. We want to parse this data and sort it into photo tags and the number
-        // of photos with a tag. We store this in photosByTags.
+        // self.photo is an NSArray of NSDictionary. We want to parse this and sort it by tag name
         for (id item in self.photos) {
             NSDictionary *photoDictionary = (NSDictionary *)item;
-            NSArray *tags = [photoDictionary[FLICKR_TAGS] componentsSeparatedByString:@" "];
+            NSArray *tokenizedTags = [photoDictionary[FLICKR_TAGS] componentsSeparatedByString:@" "];
             
-            for (NSString *tag in tags) {
-                if (!self.photosByTags[tag]) {
-                    // Add tag to our dictionary
-                    [self.photosByTags setValue:@(1) forKey:tag];
+            for (NSString *tag in tokenizedTags) {
+                if (!_tags[tag]) {
+                    [_tags setValue:@[photoDictionary] forKey:tag];
                 }
                 else
                 {
-                    NSNumber *count = self.photosByTags[tag];
-                    self.photosByTags[tag] = @(count.intValue + 1);
+                    _tags[tag] = [_tags[tag] arrayByAddingObject:photoDictionary];
                 }
             }
         }
+        
+        // We have to use a separate array to keep track of which tag corresponds to which row. We can't rely on
+        // using the allKeys method of tags because NSDictionary doesn't guarantee that it will return the same order
+        // each time it's invoked.
+        _rowTags = [self.tags allKeys];
     }
-    return _photosByTags;
+    return _tags;
 }
 
 // Tag name, capitalized
 - (NSString *)titleForRow:(NSUInteger)row
 {
-    [self.photosByTags count];
-    return @"";
+    return [[self.rowTags[row] description] capitalizedString];
 }
 
 // Number of photos with this tag
 - (NSString *)subtitleForRow:(NSUInteger)row
 {
-    return @"";
+    NSString *tagName = self.rowTags[row];
+    return [NSString stringWithFormat:@"%d photos", [self.tags[tagName] count]];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [self.photos count];
+    return [self.tags count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
